6b700c45454e9977b1155a7c9628f5581466c074
Alexandr-Solovev <n19ht1n9ale27@gmail.com>
2021-05-16 20:15:06 +0300

    add new random

--- modules/bulls-n-cows/include/BullsAndCowsApp.h
+++ modules/bulls-n-cows/include/BullsAndCowsApp.h
@@ -5,6 +5,7 @@
 
 #include <string>
 
+int generateRandomNumber();
 class BullsAndCowsApp {
  public:
     BullsAndCowsApp() = default;
--- modules/bulls-n-cows/src/BullsAndCowsApp.cpp
+++ modules/bulls-n-cows/src/BullsAndCowsApp.cpp
@@ -1,13 +1,25 @@
 // Copyright 2021 Solovev Aleksandr
 #include <algorithm>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <utility>
 #include <vector>
 
 #include "include/BullsAndCowsApp.h"
 #include "include/BullsAndCows.h"
+
+int generateRandomNumber() {
+    std::mt19937 gen;
+    int min = 0;
+    int max = 9;
+    int result;
+    gen.seed(static_cast<unsigned int>(time(0)));
+    result = static_cast<int>(gen()) % (max - min + 1) + min;
+    return abs(result);
+}
 
 std::string BullsAndCowsApp::operator()(int argc, const char* argv[],
                                int* retcode) const {
@@ -16,7 +28,6 @@ std::string BullsAndCowsApp::operator()(int argc, const char* argv[],
     if (argc != 3)
         return "invalid count of arguments, should be 2";
     std::string arg = argv[1];
-    srand(time(0));
     int count;
     try {
         count = std::stoi(arg);
@@ -27,14 +38,12 @@ std::string BullsAndCowsApp::operator()(int argc, const char* argv[],
             return "[ERROR] " + arg + ": out of range. " +
                    std::string(e.what());
         }
-    if(count <= 0)
+    if (count <= 0)
         return "invalid argument of count should be (0-9)";
     arg = argv[2];
     std::vector<int> guess(count);
-    try
-    {
-        for (int i = 0; i < count - 1; i++)
-        {
+    try {
+        for (int i = 0; i < count - 1; i++) {
             guess[i] = arg[i] - '0';
         }
     } catch (std::invalid_argument& e) {
@@ -46,15 +55,12 @@ std::string BullsAndCowsApp::operator()(int argc, const char* argv[],
         }
     int x;
     std::vector<int> answer(count);
-    for (int i = 0; i < count; i++)
-    {
-        do
-        {
-            x = rand() % 9;
+    for (int i = 0; i < count; i++) {
+        do {
+            x = generateRandomNumber();
         } while (std::find(answer.begin(), answer.end(), x) != answer.end());
-        answer[i] = x;    
+        answer[i] = x;
     }
-    
     std::string str;
     try {
         bullsAndCowsGame game(answer);
@@ -63,12 +69,10 @@ std::string BullsAndCowsApp::operator()(int argc, const char* argv[],
         std::pair<int, int> result = game.getAnimals();
         str = "Bulls:" + std::to_string(result.first) +
               " Cows:" + std::to_string(result.second);
-        std::cout<<"Answer:";
-        for (size_t i = 0; i < count; i++)
-        {
+        std::cout << "Answer:";
+        for (size_t i = 0; i < count; i++) {
            std::cout << answer[i];
         }
-        
     } catch (std::runtime_error& e) {
         return "[ERROR]" + std::string(e.what());
     }
